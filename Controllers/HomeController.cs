6c83e1234314c88a17dd71f54a183c5714032c3b
TP045997 <TP045997@mail.apu.edu.my>
2019-11-12 17:07:01 +0800

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace TestUseGitHub.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
